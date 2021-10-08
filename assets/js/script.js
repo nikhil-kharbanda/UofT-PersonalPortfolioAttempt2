261baf1f2fd4af266cfbc701a07dc10c86424dfd
Nikhil Kharbanda <nikhil_kharbanda@hotmail.com>
2021-10-08 00:06:14 -0400

    commit before restart

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,24 @@ $(document).ready(function(){
         $('.navbar .menu').toggleClass("active");
     });
 
+     // slide-up script
+     $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
     $('.carousel').owlCarousel({
         loop: false,
         margin: 20,
