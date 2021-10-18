c0968e4f3b28e0e21105f0538a1a0a62e00f969d
Nikhil Kharbanda <nikhil_kharbanda@hotmail.com>
2021-10-18 12:36:37 -0400

    dropdown feature fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,8 @@ $(document).ready(function(){
     });
 
     //Toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-    });
 
-     // slide-up script
+    // slide-up script
      $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
         // removing smooth scroll on slide-up button click
